09baeeb16cd475f43b31ddd53f174cd1e538e495
shubham <shubham@delimp.com>
2021-07-03 16:39:05 +0530

    read keys

--- app/services/thirdParty.service.js
+++ app/services/thirdParty.service.js
@@ -4,23 +4,38 @@ const path = require("path");
 const getAuth = require("../../config/getAuth");
 const NodeRSA = require('node-rsa');
 
-// with open('/path/ProductionPublicKey.txt', 'r') as file:
-let absPath = path.resolve("./ProductionPublicKey.txt");
-fs.readFile(absPath, 'utf-8' ,(err, data)=>{
-    console.log("data key",data, absPath);
-});
-// let private_key = fs.readFile("./ProductionPrivateKey.txt",()=>{});
-// let secret_key = fs.readFile("./SecretKey.txt",()=>{});
-// const key = new NodeRSA()
-// RSA.importKey(file.read())
-// console.log("public key",public_key);
-// console.log(private_key);
-// console.log(secret_key);
-// with open('/path/ProductionPrivateKey.txt', 'r') as file:
-//     private_key = RSA.importKey(file.read())
+let public_keyPath = path.resolve("./app/services/ProductionPublicKey.txt");
+let private_keyPath = path.resolve("./app/services/ProductionPrivateKey.txt");
+let secret_keyPath = path.resolve("./app/services/SecretKey.txt");
 
-// with open('/path/SecretKey.txt', 'r') as file:
-//     secret_key = file.read()
+let public_key = fs.readFile("./ProductionPrivateKey.txt",()=>{});
+let private_key = fs.readFile("./ProductionPrivateKey.txt",()=>{});
+let secret_key = fs.readFile("./SecretKey.txt",()=>{});
+
+fs.readFile(public_keyPath, 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    public_key = data;
+    // new NodeRSA(data);
+  })
+
+  fs.readFile(private_keyPath, 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    private_key = data;
+  })
+fs.readFile(secret_keyPath, 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    secret_key = data;
+  })
+
 
 
 exports.save = [
